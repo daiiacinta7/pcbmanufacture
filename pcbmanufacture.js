3adca58bf5707760ad90f912c1f041af270cf0f2
daiiacinta7 <32591374+daiiacinta7@users.noreply.github.com>
2018-09-24 23:20:14 +0700

    Update pcbmanufacture.js

--- pcbmanufacture.js
+++ pcbmanufacture.js
@@ -26,7 +26,7 @@ if(x=="2"){xxx="/2018/03/five-erp-software-trends-to-watch-in.html"}
 if(x=="3"){xxx="/2018/03/putting-e-enterprise-back-into-erp.html"}
 if(x=="4"){xxx="/2018/03/how-erp-software-may-be-affected-by-end.html"}
 if(x=="5"){xxx="/2018/03/tips-to-choose-best-erp-system.html"}
-if(x=="6"){xxx="/2017/12/how-to-find-right-pcb-manufacturer_1.html"}a_to_ve[i].href="http://www.it-inventory.review"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="6"){xxx="/2017/12/how-to-find-right-pcb-manufacturer_1.html"}a_to_ve[i].href="http://www.pcbmanufacturing.review"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
